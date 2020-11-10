881c1145d8757c48d70c6c4ae2255d71c66b425a
fedor <f.avetisov@gmail.com>
2020-11-10 16:51:36 +0300

    chore: added release command to package.json

--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const execSync = require('child_process').execSync;
 
 // transpiling scripts code to js. If this action changes working tree, you will have to commit changes manually
