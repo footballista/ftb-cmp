6769f4829b576c6173d37b01bf4f64cc496255f5
fedor <f.avetisov@gmail.com>
2020-11-11 12:37:37 +0300

    feat: ftb-user-photo created

---
+++ src/components/ftb-user-photo/ftb-user-photo.component.tsx
@@ -0,0 +1,34 @@
+import { Component, Host, h, Prop, State } from '@stencil/core';
+import { User } from 'ftb-models/dist/models/user.model';
+import Avatar from '../../assets/icons/avatar.svg';
+
+@Component({
+  tag: 'ftb-user-photo',
+  styleUrl: 'ftb-user-photo.component.scss',
+  shadow: false,
+})
+export class FtbUserPhoto {
+  @Prop() user: User; // use User model or separate properties below ↙
+  @Prop() userId: number;
+  @Prop() version: number;
+  @State() showPlaceholder: boolean = false;
+  @State() url: string;
+
+  componentWillLoad() {
+    this.url = `https://footballista.ru/api/img/users/${this.user?._id || this.userId}.jpg?version=${
+      this.user?.photoId || this.version
+    }`;
+  }
+
+  render() {
+    return (
+      <Host>
+        {this.showPlaceholder ? (
+          <ftb-icon svg={Avatar}></ftb-icon>
+        ) : (
+          <ftb-img src={this.url} onFailed={() => (this.showPlaceholder = true)}></ftb-img>
+        )}
+      </Host>
+    );
+  }
+}
